0f39c31be7602e0105a1b388f955c9bbb3ad2218
gilanghyro <49299884+gilanghyro@users.noreply.github.com>
2023-02-09 22:43:13 +0800

    final

--- dist/js/script.js
+++ dist/js/script.js
@@ -32,13 +32,25 @@ window.addEventListener("click", function (e) {
 });
 
 //darkmode
-const darToggle = document.querySelector("#dark-toggle");
+const darkToggle = document.querySelector("#dark-toggle");
 const html = document.querySelector("html");
 
 darkToggle.addEventListener("click", function () {
-  if ((darToggle, checked)) {
+  if (darkToggle.checked) {
     html.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     html.classList.remove("dark");
+    localStorage.theme = "light";
   }
 });
+
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
